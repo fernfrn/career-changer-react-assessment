231a18b30726f0b2d9995634eac9214510e330cb
fernfrn <piyaratt.sittiyos@gmail.com>
2023-04-19 12:48:33 +0700

    done for jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Home from './Home' // import home
+import Owner from './Owner' // import Owner
+import Navbar from './Navbar'
+import AutoSync from './AutoSync'
+import Input from './Input'
 import {
-  createBrowserRouter,
+  createBrowserRouter,              
   RouterProvider,
 } from "react-router-dom";
 
+//create router provider
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <>
+      <Navbar />
+      <Home />
+    </>
+  },
+
+  {
+    path: '/owner',
+    element: <>
+      <Navbar />
+      <Owner />
+    </>
+  },
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={} />
+  <RouterProvider router={router} /> //input router
 )
